1ee86c41a2b3aed75ee6a37970f045bd3d4f663a
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-10-04 06:33:51 +0530

    Comment add to code
    The code has been updated with some new comments.

--- Level1/Event_Organization/eventOrganization.sol
+++ Level1/Event_Organization/eventOrganization.sol
@@ -1,7 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.5.0 <0.9.0;
+
+//SPDX licence is introduced in Solidity 0.6.8. As a result, you must use SPDX-License-Identifier in the code.
  
 contract evnentOrganization{
+ 
+    //Here struct is used because They are useful for grouping together related data.
     struct Event{
         address organizer;
         string name;
@@ -13,11 +17,12 @@ contract evnentOrganization{
         
     //Mapping is used in this case to store a Multiple number of events.
     mapping(uint=>Event) public events;
+    
     //This is Nested Mapping, which is used to allow multiple users to participate in multiple events.
     mapping(address=>mapping(uint=>uint)) public tickets;
     uint public nextId;
  
-    
+    //It will initiate the event (The event will be created by the organiser.)
     function createEvent(string memory name,uint date,uint price,uint maxTickets) external{
         // Require is used to determine whether or not the condition is satisfied
     require(date>block.timestamp,"You can organize event for future date");
@@ -26,7 +31,7 @@ contract evnentOrganization{
     nextId++;
     }
  
-    
+    //This is the Payable function for buying event tickets.
     function buyTicket(uint id,uint quantity) external payable{
     require(events[id].date!=0,"Event does not exist");
     require(events[id].date>block.timestamp,"Event has already occured");
