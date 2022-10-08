3c8de4f5e1d1236fd02671885a3d872d8090330f
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2022-10-08 18:55:17 +0530

    Update Lib.sol

--- Level1/Library Contract/Lib.sol
+++ Level1/Library Contract/Lib.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.7;
 
 contract Library {
 
+  // adding an event to add books 
   event AddBook(address recipient, uint bookId);
+  // adding an event to set bookId etc
   event SetFinished(uint bookId, bool finished);
 
+ // struct to add properties of the book
+ 
   struct Book {
     uint id;
     string name;
@@ -20,13 +24,16 @@ contract Library {
   // Mapping of Book id to the wallet address of the user adding the new book under their name
   mapping(uint256 => address) bookToOwner;
 
+// This function will allow us to add features to our Book like its name, year etc )
+
   function addBook(string memory name, uint16 year, string memory author, bool finished) external {
     uint bookId = bookList.length;
     bookList.push(Book(bookId, name, year, author, finished));
     bookToOwner[bookId] = msg.sender;
     emit AddBook(msg.sender, bookId);
   }
-
+ 
+ // With this function we will get the list of our books
   function _getBookList(bool finished) private view returns (Book[] memory) {
     Book[] memory temporary = new Book[](bookList.length);
     uint counter = 0;
@@ -45,15 +52,16 @@ contract Library {
     }
     return result;
   }
-
+  // we will get finished status books with this function    
   function getFinishedBooks() external view returns (Book[] memory) {
     return _getBookList(true);
   }
-
+   
+   // we will get unfinished status books with this function    
   function getUnfinishedBooks() external view returns (Book[] memory) {
     return _getBookList(false);
   }
-
+  // allows us to set the status of our book as finished or unfinished
   function setFinished(uint bookId, bool finished) external {
     if (bookToOwner[bookId] == msg.sender) {
       bookList[bookId].finished = finished;
