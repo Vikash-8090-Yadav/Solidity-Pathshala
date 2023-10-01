0c8a06678ee60756d7b3840a79273977908d51a0
rohit <rhtry.tech@gmail.com>
2023-10-01 13:39:17 +0530

    improve : Banking smart contract for gas efficiency

--- Level1/Basic-Banking/BasicBanking.sol
+++ Level1/Basic-Banking/BasicBanking.sol
@@ -1,70 +1,102 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 <0.7.0;
 
-contract banking{
-  mapping(address=>uint) public userAccount;
-  mapping(address=>bool) public userExists;
+contract Banking {
+    mapping(address => uint) public userAccount;
+    mapping(address => bool) public userExists;
 
-// can create the account 
-  function createAcc() public payable returns(string memory){
-      require(userExists[msg.sender]==false,'Account Already Created');
-      if(msg.value==0){
-          userAccount[msg.sender]=0;
-          userExists[msg.sender]=true;
-          return 'account created';
-      }
-      require(userExists[msg.sender]==false,'account already created');
-      userAccount[msg.sender] = msg.value;
-      userExists[msg.sender] = true;
-      return 'account created';
-  }
-  // cvan deposit amount in his account
-  function deposit() public payable returns(string memory){
-      require(userExists[msg.sender]==true, 'Account is not created');
-      require(msg.value>0, 'Value for deposit is Zero');
-      userAccount[msg.sender]=userAccount[msg.sender]+msg.value;
-      return 'Deposited Succesfully';
-  }
+    // Can create a new bank account
+    function createAcc() public payable returns (string memory) {
+        require(!userExists[msg.sender], "Account already created !");
 
-  // can withdraw amount from  his acocunt
-  
-  function withdraw(uint amount) public payable returns(string memory){
-      require(userAccount[msg.sender]>amount, 'insufficeint balance in Bank account');
-      require(userExists[msg.sender]==true, 'Account is not created');
-      require(amount>0, 'Enter non-zero value for withdrawal');
-      userAccount[msg.sender]=userAccount[msg.sender]-amount;
-      msg.sender.transfer(amount);
-      return 'withdrawal Succesful';
-  }
- 
-  function TransferAmount(address payable userAddress, uint amount) public returns(string memory){
-      require(userAccount[msg.sender]>amount, 'insufficeint balance in Bank account');
-      require(userExists[msg.sender]==true, 'Account is not created');
-      require(userExists[userAddress]==true, 'to Transfer account does not exists in bank accounts ');
-      require(amount>0, 'Enter non-zero value for sending');
-      userAccount[msg.sender]=userAccount[msg.sender]-amount;
-      userAccount[userAddress]=userAccount[userAddress]+amount;
-      return 'transfer succesfully';
-  }
-  
-  // can send amount to anyother person
-  function sendAmount(address payable toAddress , uint256 amount) public payable returns(string memory){
-      require(amount>0, 'Enter non-zero value for withdrawal');
-      require(userExists[msg.sender]==true, 'Account is not created');
-      require(userAccount[msg.sender]>amount, 'insufficeint balance in Bank account');
-      userAccount[msg.sender]=userAccount[msg.sender]-amount;
-      toAddress.transfer(amount);
-      return 'transfer success';
-  }
-  
-  // can check  balance
-  function userAccountBalance() public view returns(uint){
-      return userAccount[msg.sender];
-  }
-  
-  // check is the account registered to the bank or not 
-  function accountExist() public view returns(bool){
-      return userExists[msg.sender];
-  }
-  
+        if (msg.value > 0) {
+            userAccount[msg.sender] = msg.value;
+            userExists[msg.sender] = true;
+            return "Account created with a deposit";
+        }
+
+        userExists[msg.sender] = true;
+        return "Account created without a deposit";
+    }
+
+    // Can deposit amount in his/her account
+    function deposit() public payable returns (string memory) {
+        require(userExists[msg.sender], "Account is not created");
+        require(msg.value > 0, "Value for deposit is Zero");
+
+        userAccount[msg.sender] += msg.value;
+
+        return "Deposited successfully";
+    }
+
+    // Can withdraw amount from  his acocunt
+    modifier validWithdrawal(uint amount) {
+        require(amount > 0, "Enter a non-zero value for withdrawal");
+        require(userExists[msg.sender], "Account is not created");
+        require(
+            userAccount[msg.sender] >= amount,
+            "Insufficient balance in the bank account"
+        );
+        _;
+    }
+
+    function withdraw(
+        uint amount
+    ) public payable validWithdrawal(amount) returns (string memory) {
+        userAccount[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        return "Withdrawal Successful";
+    }
+
+    // Can transfer amount from your account to another
+    function transferAmount(
+        address payable userAddress,
+        uint amount
+    ) public returns (string memory) {
+        require(userExists[msg.sender], "Account is not created");
+        require(
+            userExists[userAddress],
+            "Recipient account does not exist in bank accounts"
+        );
+        require(amount > 0, "Enter a non-zero value for sending");
+        require(
+            userAccount[msg.sender] >= amount,
+            "Insufficient balance in Bank account"
+        );
+
+        userAccount[msg.sender] -= amount;
+        userAccount[userAddress] += amount;
+
+        return "Transfer successful";
+    }
+
+    // Can send amount to another person
+    function sendAmount(
+        address payable toAddress,
+        uint256 amount
+    ) public returns (string memory) {
+        require(amount > 0, "Enter a non-zero value for withdrawal");
+        require(userExists[msg.sender], "Account is not created");
+
+        uint256 senderBalance = userAccount[msg.sender];
+        require(
+            senderBalance >= amount,
+            "Insufficient balance in Bank account"
+        );
+
+        userAccount[msg.sender] -= amount;
+        toAddress.transfer(amount);
+
+        return "Transfer successful";
+    }
+
+    // Can check  balance
+    function userAccountBalance() public view returns (uint) {
+        return userAccount[msg.sender];
+    }
+
+    // Can check is the account registered to the bank or not
+    function accountExist() public view returns (bool) {
+        return userExists[msg.sender];
+    }
 }
