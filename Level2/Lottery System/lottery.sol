f1de1cc187d540c7992d1983503096827c6094b7
@Yash9276 <yashkudnar519@gmail.com>
2023-10-04 13:57:15 +0530

    addidng functions

--- Level2/Lottery System/lottery.sol
+++ Level2/Lottery System/lottery.sol
@@ -4,11 +4,17 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-error Raffle__SendMoreToEnterRaffle();
+error Lottery__SendMoreToEnterLottery();
+error   Lottery__LotteryNotOpen();
 
 contract Lottery is is VRFConsumerBaseV2{
     // address public manager;
     // address payable[] public participants;
+
+    enum LotteryState {
+    OPEN,
+    CALCULATING
+  }
     
   
 
@@ -26,11 +32,11 @@ contract Lottery is is VRFConsumerBaseV2{
   uint256 private s_lastTimeStamp;
   address private s_recentWinner;
   address payable[] private s_players;
-  RaffleState private s_raffleState;
+  LotteryState private s_LotteryState;
 
     // Events 
-  event RequestedRaffleWinner(uint256 indexed requestId);
-  event RaffleEnter(address indexed player);
+  event RequestedLotteryWinner(uint256 indexed requestId);
+  event LotteryEnter(address indexed player);
   event WinnerPicked(address indexed player);
 
   constructor(
@@ -47,15 +53,87 @@ contract Lottery is is VRFConsumerBaseV2{
     i_interval = interval;
     i_subscriptionId = subscriptionId;
     i_entranceFee = entranceFee;
-    s_raffleState = RaffleState.OPEN;
+    s_LotteryState = LotteryState.OPEN;
     s_lastTimeStamp = block.timestamp;
     i_callbackGasLimit = callbackGasLimit;
     manager=msg.sender;
   }
-
-
+  // Defining functions
         modifier onlyManager() {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
+
+    //Enter  Lottery function with revert rathar than require which uses relativelt more gas
+
+    function enterLottery() public payable {
+    // require(msg.value >= i_entranceFee, "Not enough value sent");
+    // require(s_LotteryState == LotteryState.OPEN, "Lottery is not open");
+    if (msg.value < i_entranceFee) {
+      revert Lottery__SendMoreToEnterLottery();
+    }
+    if (s_LotteryState != LotteryState.OPEN) {
+      revert Lottery__LotteryNotOpen();
+    }
+    s_players.push(payable(msg.sender));
+    emit LotteryEnter(msg.sender);
+  }
+
+// In this function we check that we need to conduct lottery game or not
+  function checkUpkeep(
+    bytes memory /* checkData */
+  )
+    public
+    view
+    override
+    returns (bool upkeepNeeded, bytes memory /* performData */)
+  {
+    bool isOpen = LotteryState.OPEN == s_LotteryState;
+    bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+    bool hasPlayers = s_players.length > 0;
+    bool hasBalance = address(this).balance > 0;
+    upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
+    return (upkeepNeeded, "0x0"); // can we comment this out?
+  }
+
+  
+  function performUpkeep(bytes calldata /* performData */) external override {
+    (bool upkeepNeeded, ) = checkUpkeep("");
+    if (!upkeepNeeded) {
+      revert Lottery__UpkeepNotNeeded(
+        address(this).balance,
+        s_players.length,
+        uint256(s_LotteryState)
+      );
+    }
+    s_LotteryState = LotteryState.CALCULATING;
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane,
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
+    emit RequestedLotteryWinner(requestId);
+  }
+
+
+  function fulfillRandomWords(
+    uint256 /* requestId */,
+    uint256[] memory randomWords
+  ) internal override {
+
+    uint256 indexOfWinner = randomWords[0] % s_players.length;
+    address payable recentWinner = s_players[indexOfWinner];
+    s_recentWinner = recentWinner;
+    s_players = new address payable[](0);
+    s_LotteryState = LotteryState.OPEN;
+    s_lastTimeStamp = block.timestamp;
+    (bool success, ) = recentWinner.call{ value: address(this).balance }("");
+    if (!success) {
+      revert Lottery__TransferFailed();
+    }
+    emit WinnerPicked(recentWinner);
+  }
+
 }
