e46a30aad233a95b30bc22e72c30649fe7404214
@Yash9276 <yashkudnar519@gmail.com>
2023-10-04 13:59:52 +0530

    addidng getter  functions

--- Level2/Lottery System/lottery.sol
+++ Level2/Lottery System/lottery.sol
@@ -135,5 +135,40 @@ contract Lottery is is VRFConsumerBaseV2{
     }
     emit WinnerPicked(recentWinner);
   }
+  //adding getter functions
+    function getRaffleState() public view returns (RaffleState) {
+    return s_raffleState;
+  }
 
+  function getNumWords() public pure returns (uint256) {
+    return NUM_WORDS;
+  }
+
+  function getRequestConfirmations() public pure returns (uint256) {
+    return REQUEST_CONFIRMATIONS;
+  }
+
+  function getRecentWinner() public view returns (address) {
+    return s_recentWinner;
+  }
+
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
+
+  function getLastTimeStamp() public view returns (uint256) {
+    return s_lastTimeStamp;
+  }
+
+  function getInterval() public view returns (uint256) {
+    return i_interval;
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
+
+  function getNumberOfPlayers() public view returns (uint256) {
+    return s_players.length;
+  }
 }
